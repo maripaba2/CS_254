903ed802b41132560be56b17beff3152f85abe29
maripaba2 <135129079+maripaba2@users.noreply.github.com>
2024-01-21 19:35:10 +0530

    Update Q2.cpp

--- LAB2/Q2/Q2.cpp
+++ LAB2/Q2/Q2.cpp
@@ -1,48 +1,43 @@
 #include <iostream>
-using namespace std;
 
-void merge(int A[], int start, int mid, int end) {
-    int p = start, q = mid + 1;
-    if (A[mid] <= A[q]) {
-        return;  
-    }
-    while (p <= mid && q <= end) {
-        if (A[p] <= A[q]) {
-            p++;
+void inPlaceMerge(int A[], int m, int n) {
+    int i = 0;
+    int j = m;
+    int k = 0;
+
+    while (i < m && j < n + m) {
+        if (A[i] <= A[j]) {
+            A[k] = A[i];
+            i++;
         } else {
-            int temp = A[q];
-
-            for (int i = q; i > p; i--) {
-                A[i] = A[i - 1];
-            }
-
-            A[p] = temp;
-            p++;
-            mid++;
-            q++;
+            A[k] = A[j];
+            j++;
         }
+        k++;
     }
-}
 
-void mergeSort(int A[], int start, int end) {
-    if (start < end) {
-        int mid = start +(end-start)/2;
-        mergeSort(A, start, mid);
-        mergeSort(A, mid + 1, end);
-        merge(A, start, mid, end);
+    while (i < m) {
+        A[k] = A[i];
+        i++;
+        k++;
+    }
+
+    while (j < n + m) {
+        A[k] = A[j];
+        j++;
+        k++;
     }
 }
 
 int main() {
-    int n;
-    cin >> n;
+    int m,n;cin>>m>>n;
     int A[n];
-    for (int i = 0; i < n; i++) {
-        cin >> A[i];
-    }
-    mergeSort(A, 0, n - 1);        // time complexity O(nlog(n))
-    for (int i = 0; i < n; i++) {
-        cout << A[i] << " ";
+    for(int i=0;i<n;i++)cin>>A[i];
+    
+    inPlaceMerge(A, m, n);
+
+    for (int i = 0; i < m + n; i++) {
+        std::cout << A[i] << " ";
     }
 
     return 0;
